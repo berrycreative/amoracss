07b2809f085c353cde27837d985772b5b223a155
Quen-Quen Potter <quenquen@outlook.com>
2024-09-09 15:24:49 -0300

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,6 +1,6 @@
 function switchMode(el) {
   const bodyClass = document.body.classList;
   bodyClass.contains('dark')
-    ? (el.innerHTML = '<span class="ico sun" style="font-size: 24px;"></span>', bodyClass.remove('dark'))
-    : (el.innerHTML = '<span class="ico moon" style="font-size: 24px;"></span>', bodyClass.add('dark')); 
+    ? (el.innerHTML = '<span class="ico sun" style="font-size: 2.25rem;"></span>', bodyClass.remove('dark'))
+    : (el.innerHTML = '<span class="ico moon" style="font-size: 2.25rem;"></span>', bodyClass.add('dark')); 
 }
